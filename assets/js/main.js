913ff2a1ced38b3c8ba6bd3bf5421c3b0dfd30d9
alinbrg <alina.bregvadze@gmail.com>
2022-10-20 15:07:00 +0400

    added doctors and news sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,24 @@ function heroSlider() {
 		spaceBetween: 10,
 	});
 }
+function mainDoctorsSlider() {
+	const swiper = new Swiper(".main_doctors-swiper", {
+		breakpoints: {
+			320: {
+				slidesPerView: 1.4,
+				spaceBetween: 12,
+			},
+			768: {
+				slidesPerView: 3,
+				spaceBetween: 12,
+				grabCursor: true,
+			},
+			1024: {
+				slidesPerView: 4,
+				spaceBetween: 16,
+			},
+		},
+	});
+}
 heroSlider();
+mainDoctorsSlider();
